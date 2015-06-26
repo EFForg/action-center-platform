dec8b0149915b7f5362aa1888acfdfdd54fa1e0f
William Budington <bill@eff.org>
2015-06-25 17:46:33 -0700

    Don't send to bounce addresses

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,21 @@ class UserMailer < ActionMailer::Base
   helper ApplicationHelper
   helper ActionPageHelper
   layout 'email'
+  after_filter :check_bounces
 
   def thanks_message(email, actionPage, options={})
+    @email = email
     @user = options[:user]
     @actionPage = actionPage
     @name = options[:name]
     mail(to: email, subject: 'Thanks for taking action')
+  end
+
+  private
+
+  def check_bounces
+    unless Bounce.find_by_email(@email.downcase).nil?
+      mail.perform_deliveries = false
+    end
   end
 end
