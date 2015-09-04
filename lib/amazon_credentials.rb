0a108475c26d3bddbe570bb22ae239255d82267e
William Budington <bill@eff.org>
2015-09-03 17:52:50 -0700

    Allow config file to specify a bucket URL for AWS

--- lib/amazon_credentials.rb
+++ lib/amazon_credentials.rb
@@ -1,5 +1,12 @@
 module AmazonCredentials
   def amazon_credentials
+    bucket_url_options = {}
+    bucket_url_options = {
+      :s3_host_alias => Rails.application.secrets.amazon_bucket_url,
+      :url => ':s3_alias_url',
+      :path => '/:class/:attachment/:id_partition/:style/:filename'
+    } unless Rails.application.secrets.amazon_bucket_url.nil?
+
     {
       :storage => Rails.application.secrets.storage.to_sym,
       :s3_credentials => {
@@ -9,6 +16,6 @@ module AmazonCredentials
       },
       :s3_host_name => 's3-' + Rails.application.secrets.amazon_region + '.amazonaws.com',
       :s3_protocol => 'https'
-    }
+    }.merge(bucket_url_options)
   end
 end
