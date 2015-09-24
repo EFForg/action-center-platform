43f4e055f7e4bb9a75838ce21b71237e6ee10f3b
TheNotary <no@email.plz>
2015-09-24 10:56:53 -0700

    pulled out javascript content strings into it's own file.

---
+++ app/assets/javascripts/application/strings.js
@@ -0,0 +1,6 @@
+window.App = window.App || {};
+
+window.App.Strings = {
+  invalidEmailSubmission: "<p>Ok... kinda bad news...<p></p>Something went wrong.  Maybe the email you gave us had a typo???  Please try again.</p>",
+  successfulEmailSubmission: "<p>Thanks for subscribing, you're awesome!<p></p>Check your email for a confirmation link.</p>"
+};
