8303499132c3eab0d9b4652ab00db0e49079e667
William Budington <bill@eff.org>
2016-05-20 14:41:03 -0700

    Adding column to petition indicating whether we want to show all sigs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107220954) do
+ActiveRecord::Schema.define(version: 20160520212818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,6 +173,7 @@ ActiveRecord::Schema.define(version: 20160107220954) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "goal"
+    t.boolean  "show_all_signatures", default: false
   end
 
   create_table "signatures", force: :cascade do |t|
