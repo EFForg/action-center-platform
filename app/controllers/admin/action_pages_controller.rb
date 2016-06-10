3e4983baff426c5822737fba8e3f8c1dcda914e3
vivian <vivian@eff.org>
2016-06-10 16:38:03 -0600

    Revert "Add institutions to the admin nav."
    This reverts commit 2f67561174051d5393da9799a083e397d22eaf85.

--- app/controllers/admin/action_pages_controller.rb
+++ app/controllers/admin/action_pages_controller.rb
@@ -9,7 +9,6 @@ class Admin::ActionPagesController < Admin::ApplicationController
     @featuredActionPages = FeaturedActionPage.order('weight').
                                               includes(:action_page).
                                               map(&:action_page)
-    @action_tab_active = 'active'
   end
 
   def update_featured_pages
