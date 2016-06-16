7fde12d1bbeb46f44a074011352655cbcf55d6cb
squiggy <squiggy@eff.org>
2016-06-16 13:33:38 -0700

    Add test for downloading signatures.

--- features/action_page/show_all_signatures.feature
+++ features/action_page/show_all_signatures.feature
@@ -1,8 +1,13 @@
 @javascript
-Feature: Show all signatures
+Feature: Show all signatures and allow download of signatures
   An interested member of the community
-  Should be able to see all signatures on a petition
+  Should be able to see all signatures on a petition 
+  And download signatures as a CSV file
 
   Scenario: A user views a petition and they should see all signatures
     Given A petition exists and show all signatures is enabled
-    When I visit an action page I should see all signatures
+    When I visit an action page
+    Then I should see "All Signatures"
+    And I should see "Download CSV"
+    When I click "Download CSV"
+    Then I should receive a CSV file
--- features/step_definitions/action_page_steps.rb
+++ features/step_definitions/action_page_steps.rb
@@ -1,11 +1,15 @@
 Given(/^A petition exists and show all signatures is enabled$/) do
   @action_page = FactoryGirl.create(:petition_show_all_signatures).action_page
-
   expect(@action_page.petition.show_all_signatures).to be_truthy
 end
 
-When(/^I visit an action page I should see all signatures$/) do
+When(/^I visit an action page$/) do
   visit "/action/#{@action_page.title.downcase.gsub(" ", "-")}"
-  expect(@action_page.petition.show_all_signatures).to be_truthy
   expect(page).to have_content "All Signatures"
+end
+
+Then(/^I should receive a CSV file$/) do
+  current_path.should == "/petition/#{@action_page.petition.id}/signatures.csv"
+  page.response_headers['Content-Type'].should == "application/octet-stream"
+  page.response_headers['Content-Disposition'].should == "attachment"
 end
