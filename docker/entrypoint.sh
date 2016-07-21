3b37d64464171db9ec1d937bc7e4983ed3a44bc1
vivian <vivian@eff.org>
2016-07-21 11:23:36 -0700

    Precompile assets in docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,8 @@ if [ "$TESTDB_ENV_AUTO_MIGRATE" == "yes" ]; then
   bin/rake db:schema:load RAILS_ENV=test
 fi
 
+bin/rake assets:precompile
+
 # host ip needed by application for better_errors whitelisting to work
 export HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
 
