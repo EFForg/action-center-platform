fdf10a9f03e54a5b2d42fcc0b31a73bf81aaf17c
vivian <vivian@eff.org>
2016-07-26 12:18:33 -0400

    Make long select2 menu more responsive by paging options

--- app/assets/javascripts/application/affiliations.js
+++ app/assets/javascripts/application/affiliations.js
@@ -2,7 +2,7 @@ $(document).on('ready', function() {
 
   var $affiliations = $('#affiliations');
 
-  // Users must input complete institution/relationship pairs.
+  // Users signing the petition must input complete institution/relationship pairs.
   $affiliations.on('change', 'select', function() {
     var select_pair = $(this).closest('.nested-fields').find('select');
     var at_least_one_selected = _.reduce(select_pair, function(m, n) {
@@ -11,46 +11,38 @@ $(document).on('ready', function() {
     select_pair.prop('required', at_least_one_selected);
   });
 
-  // Greyed-out placeholder text
+  // Greyed-out placeholder text for selects
   $affiliations.on('change', 'select', function() {
     $(this).toggleClass("empty", $.inArray($(this).val(), ['', null]) >= 0);
   })
 
-  function affiliation_added() {
-    // Autocomplete select using select2
-    $('#affiliations select.institution').select2({
-      theme: 'bootstrap',
-      placeholder: 'Institution'
-    }).on("select2:close", function (e) {
-      // focus the next element when the select2 is closed
-      $('.affiliation-type').focus();
-    });
-    height_changed();
+  select2_options = {
+    theme: 'bootstrap',
+    placeholder: 'Institution',
+    dataAdapter: $.fn.select2.amd.require('select2/data/pagedAdapter'),
+    jsonData: institutions,
+    jsonMap: {id: "id", text: "name"},
+    ajax: {}
   }
 
   $affiliations.on('cocoon:after-insert', function(e, insertedItem) {
     affiliation_added();
   }).trigger('cocoon:after-insert');
 
-  // Autocomplete filter by institution
-  $('#signatures select.institution').select2({
-    theme: 'bootstrap',
-    placeholder: 'Filter by institution'
-  });
+  function affiliation_added() {
+    // Autocomplete institution when signing petition.
+    $('#affiliations select.institution').select2(select2_options)
+      .on("select2:close", function (e) {
+        // Focus the next element when the select2 is closed.
+        $('.affiliation-type').focus();
+      });
+    height_changed();
+  }
 
-  // Open autocomplete on keypress
-  $('body').on('keypress', '.select2-container--focus', function(e) {
-    if (e.which >= 32) {
-      var $select = jQuery(this).prev();
-      $select.select2('open');
-
-      var $search = $select.data('select2').dropdown.$search || $select.data('select2').selection.$search;
-      $search.val(String.fromCharCode(e.which));
-      // Timeout seems to be required for Blink
-      setTimeout(function() {
-        $search.get(0).setSelectionRange(1,1);
-        $search.trigger('keyup');
-      },0 );
-    }
-  });
+  // Autocomplete institution when filtering signatures.
+  $('#signatures select.institution').select2(
+    $.extend(select2_options, {
+      placeholder: 'Filter by institution'
+    })
+  );
 });
