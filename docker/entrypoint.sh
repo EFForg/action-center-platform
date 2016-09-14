c3b9e1a9025fd467f61aa591ce098a607d455f2d
vivian <vivian@eff.org>
2016-09-13 17:18:34 -0700

    Precompile assets in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,7 +11,11 @@ if [ "$TESTDB_ENV_AUTO_MIGRATE" == "yes" ]; then
   bin/rake db:migrate RAILS_ENV=test
 fi
 
+# Download and precompile assets
 bin/rake webshims:update_public
+if [ "$RAILS_ENV" == "production" ]; then
+  bin/rake assets:precompile
+fi
 
 # host ip needed by application for better_errors whitelisting to work
 export HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
