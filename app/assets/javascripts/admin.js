79a966fb4ae5ce61085bddb1435b3799aba9ac1f
Peter Woo <peterw@eff.org>
2016-09-19 13:56:45 -0700

    Remove inline scripts from email date/congress tabulations

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -125,6 +125,64 @@ $(document).on('ready', function() {
     }, 10000);
   }
 
+
+  $("table#email_tabulation_by_date").each(function() {
+    $.ajax({
+      url: this.dataset.fills_url,
+      success: function(data) {
+        var tbody_html;
+        var total_count = 0;
+        _.each(data, function(v, k){
+          tbody_html += JST['admin/backbone/templates/email_campaign/date_tabulation_row']({
+            date: moment(k, "YYYY-MM-DD").format("YYYY-MM-DD"),
+            count: v
+          });
+          total_count += v;
+        });
+        tbody_html += JST['admin/backbone/templates/email_campaign/date_tabulation_row']({
+          date: "Total",
+          count: total_count
+        });
+        $("#email_tabulation_by_date tbody").html(tbody_html);
+      }
+    });
+  });
+
+  $("table#email_tabulation_by_congress").each(function(){
+    var table = this;
+    Step(
+      function fetchBreakdownAndNames(){
+        ajax_get(table.dataset.fills_url, this.parallel());
+        ajax_get(table.dataset.legislators_url, this.parallel());
+      },
+      function processAndDisplay(e, breakdown, names){
+        if(e) return console.log(e);
+
+        var members_sorted = _.keys(breakdown).sort();
+
+        var tbody_html;
+        var total_count = 0;
+
+        _.each(members_sorted, function(bioid){
+          tbody_html += JST['admin/backbone/templates/email_campaign/congress_tabulation_row']({
+            bioguide_id: bioid,
+            staffer_report_url: table.dataset.staffer_report_url.replace('placeholder', bioid),
+            name: names[bioid].name,
+            count: breakdown[bioid]
+          });
+          total_count += breakdown[bioid];
+        });
+        tbody_html += JST['admin/backbone/templates/email_campaign/congress_tabulation_row']({
+          bioguide_id: "",
+          staffer_report_url: "",
+          name: "Total",
+          count: total_count
+        });
+        $("#email_tabulation_by_congress tbody").html(tbody_html);
+      }
+    );
+  });
+
   var preview_button = $('#action-page-preview').click(function() {
     $('#action-page-form').clone().attr('action', preview_button.attr('href')).
                                    attr('target', '_blank').
