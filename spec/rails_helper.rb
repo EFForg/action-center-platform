f22ba75bae674d85663c0e77c1399f7a1cbbb3da
Peter Woo <peterw@eff.org>
2016-09-20 11:10:36 -0700

    Make specs more deterministic by clearing rails cache each time

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,10 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:each) do
+    Rails.cache.clear
+  end
 end
 
 # for request tests
