b0ccab37451480a9dbe68bcf3e98bb39f30bca60
Peter Woo <peterw@eff.org>
2016-09-27 11:38:44 -0700

    Silence console.log messages during tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,7 +9,8 @@ require 'cucumber/rspec/doubles'
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, url_blacklist: ["anon-stats.eff.org"])
+  Capybara::Poltergeist::Driver.new(app, url_blacklist: ["anon-stats.eff.org"],
+                                    phantomjs_logger: File.open("/dev/null", "w"))
 end
 
 Capybara.javascript_driver = :poltergeist
