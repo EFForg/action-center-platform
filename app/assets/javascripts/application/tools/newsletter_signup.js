31204228d5180de6b206bea105d748bbab60777f
Peter Woo <peterw@eff.org>
2016-09-30 17:18:33 -0700

    Try to toggle subscription[email] field required according to whether opt-in is checked

--- app/assets/javascripts/application/tools/newsletter_signup.js
+++ app/assets/javascripts/application/tools/newsletter_signup.js
@@ -13,4 +13,18 @@ $(document).on("ready", function() {
   });
 
   $("form.newsletter-subscription input").attr("disabled", null);
+
+  $("input[type=radio][name=subscribe]").on("change", function() {
+    var form = $(this).parents("form");
+    var subscribe = form.find("input[name=subscribe]:checked").val() == "1";
+    var emailField = form.find("input[name=subscription\\[email\\]]");
+    emailField.attr("required", subscribe ? "required" : null);
+  });
+
+  $("input[name=subscribe]:checked").each(function() {
+    var subscribe = $(this).val() == "1";
+    var emailField = $("input[name=subscription\\[email\\]]");
+    emailField.attr("required", subscribe ? "required" : null);
+  });
 });
+
