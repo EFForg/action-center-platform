23a266f0643f7a98d483d5bd129ff92cf455aa5d
vivian <vivian@eff.org>
2016-10-04 16:47:34 -0700

    Resize the window when the sidebar grows.

--- app/assets/javascripts/application/height_changed.js
+++ app/assets/javascripts/application/height_changed.js
@@ -1,5 +1,9 @@
 var height_changed = function(){
+  // If using scroll effect, adjust the window height so the sidebar fits.
+  if (window.scroller) window.scroller();
+
+  // For embeds, notify the parent window so it can resize the iframe.
   var container = document.querySelector("div.container");
   var height = Math.max( container.scrollHeight, container.offsetHeight );
-  window.parent.postMessage(height, "*"); 
+  window.parent.postMessage(height, "*");
 }
