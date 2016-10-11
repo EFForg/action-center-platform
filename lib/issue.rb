a66534c90103ace2efd8505db4b068c2ebbd9ce0
Peter Woo <peterw@eff.org>
2016-10-11 10:54:28 -0700

    Initialize issues after loading

--- lib/issue.rb
+++ lib/issue.rb
@@ -1,7 +1,7 @@
 class Issue < Struct.new(:title)
   def self.all
     if File.exist? Rails.root.join("config/issues.yml")
-      YAML.load_file(Rails.root.join("config/issues.yml")).sort
+      YAML.load_file(Rails.root.join("config/issues.yml")).sort.map{ |issue| new(issue) }
     else
       []
     end
