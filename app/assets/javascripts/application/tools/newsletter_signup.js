9bc9cbfe2af8efc0027e1f2c20bf285ad46a0bd6
Peter Woo <peterw@eff.org>
2016-10-13 15:05:43 -0700

    Apply class to submitted form instead of altering element styles

--- app/assets/javascripts/application/tools/newsletter_signup.js
+++ app/assets/javascripts/application/tools/newsletter_signup.js
@@ -4,9 +4,9 @@ $(document).on("ready", function() {
 
   $("form.newsletter-subscription").on("ajax:complete", function(xhr, data, status) {
     if (status == "success") {
-      $(this).find(".radio-inline, input").attr("disabled", true);
-      $(this).find("input, .radio-inline").css("opacity", 0.5);
-      $(this).find("input[type=submit]").css("background-color", "#337ab7").val("Subscribed!");
+      $(this).addClass("submitted");
+      $(this).find("input").attr("disabled", true);
+      $(this).find("input[type=submit]").val("Subscribed!");
       $(this).find(".privacy-notice-header em").text("Thanks for signing up!");
       $(this).attr("data-signed-up-for-mailings", "true");
     }
