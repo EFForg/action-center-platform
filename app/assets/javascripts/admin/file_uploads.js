54555f3bf34707d94a3b8c27e20701835b652a25
Peter Woo <peterw@eff.org>
2016-11-17 11:51:23 -0800

    Load gallery images when modal is opened

--- app/assets/javascripts/admin/file_uploads.js
+++ app/assets/javascripts/admin/file_uploads.js
@@ -15,17 +15,23 @@ $(function () {
       sequentialUploads: true
     });
 
-    // Get list of all uploaded images and populate the gallery with refs to them
-    $.getJSON('/admin/source_files', function (files) {
-      $.each(files, function(index, value) {
-        // The template it's using seems to be...
-        // app/views/admin/action_pages/_template_uploaded.html.erb
-        $('#upload_files tbody').append(tmpl('template-uploaded', value));
+    $("#myModal").on("show.bs.modal", function() {
+      $("#myModal").off("show.bs.modal");
+
+      // Get list of all uploaded images and populate the gallery with refs to them
+      $.getJSON('/admin/source_files', function (files) {
+        $("#myModal .loading-indicator").hide();
+        $.each(files, function(index, value) {
+          // The template it's using seems to be...
+          // app/views/admin/action_pages/_template_uploaded.html.erb
+          $('#upload_files tbody').append(tmpl('template-uploaded', value));
+        });
+
+        height_changed();
       });
     });
 
   }
-  height_changed();
 });
 
 // used by the jQuery File Upload
