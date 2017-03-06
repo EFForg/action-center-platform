81be09afceaba6bf59eeeb289cc7b1f40514f262
Peter Woo <peterw@eff.org>
2017-03-06 11:30:37 -0800

    Show error message to user when rep lookup fails

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -200,7 +200,7 @@ class ToolsController < ApplicationController
 
       render :json => {content: render_to_string(partial: 'action_page/reps')}, :status => 200
     else
-      render :json => {error: 'No representatives found'}, :status => 204
+      render :json => {error: 'No representatives found'}, :status => 200
     end
   end
 
@@ -213,7 +213,7 @@ class ToolsController < ApplicationController
     if @reps.present?
       render :json => @reps, :status => 200
     else
-      render :json => {error: 'No representatives found'}, :status => 204
+      render :json => {error: 'No representatives found'}, :status => 200
     end
   end
 
