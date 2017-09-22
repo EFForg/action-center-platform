f082886f5e06755e6dae01124aee0c37c21f31a5
Peter Woo <peterw@eff.org>
2017-09-22 11:39:34 -0700

    Fix specs

--- spec/controllers/congress_messages_controller_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CongressMessagesControllerController, type: :controller do
-
-end
---
+++ spec/controllers/phantom_dc_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PhantomDcController, type: :controller do
+
+end
