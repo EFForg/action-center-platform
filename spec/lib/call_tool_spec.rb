e8c6602569bbf51c74e7a34395aff7d82793efa5
Peter Woo <peterw@eff.org>
2017-11-28 17:29:06 -0800

    Fix call tool spec

--- spec/lib/call_tool_spec.rb
+++ spec/lib/call_tool_spec.rb
@@ -96,7 +96,7 @@ describe CallTool do
 
     before do
       stub_request(:get, %r{/api/campaign\?api_key=}).
-        to_return(status: 200, body: { "objects" => [calltool_campaign] }.to_json)
+        to_return(status: 200, body: { "objects" => [calltool_campaign], "page" => 1, "total_pages" => 1 }.to_json)
     end
 
     it "should get call_tool_url/api/campaign and return values with id, name, status" do
