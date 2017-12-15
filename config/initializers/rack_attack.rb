cebc2d9c3a823ad1a192343fe55be225c71057d8
vivian <vivian@eff.org>
2017-12-15 13:26:09 -0800

    Use rack_attack's default throttle response

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -10,12 +10,4 @@ class Rack::Attack
       req.env['HTTP_X_FORWARDED_FOR'].split(/\s*,\s*/)[0]
     end
   end
-
-  ### Custom Throttle Response ###
-
-  self.throttled_response = lambda do |env|
-    ip = env['HTTP_X_FORWARDED_FOR'].split(/\s*,\s*/)[0]
-#    Raven.capture_message("registration rate limit exceeded", extra: { ip: ip })
-    [ 302, { "Location" => "/register" }, [''] ]
-  end
 end
