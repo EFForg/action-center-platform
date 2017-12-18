5031109210e09329f5a473e050fbee8e99338da6
vivian <vivian@eff.org>
2017-12-18 14:13:28 -0800

    Just log to sentry

--- config/initializers/rack_attack_logger.rb
+++ config/initializers/rack_attack_logger.rb
@@ -1,7 +1,5 @@
-LOGGER = Logger.new("log/rack-attack.log")
 ActiveSupport::Notifications.subscribe('rack.attack') do |name, start, finish, request_id, req|
   message = "Rate limit exceeded on #{req.fullpath}"
   ip = req.ip || req.env['HTTP_X_FORWARDED_FOR'].ip.split(/\s*,\s*/)[0]
-  LOGGER.info(message)
   Raven.capture_message(message, extra: { ip: ip })
 end
