3e374c04e45c667cf437836f4fa81957954f041f
vivian <vivian@eff.org>
2017-12-18 13:54:24 -0800

    Remove unnecessary return

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 class Rack::Attack
   throttle('registrations', limit: 10, period: 1.day) do |req|
     if req.path == '/' && req.post?
-      return req.ip || req.env['HTTP_X_FORWARDED_FOR'].ip.split(/\s*,\s*/)[0]
+      req.ip || req.env['HTTP_X_FORWARDED_FOR'].ip.split(/\s*,\s*/)[0]
     end
   end
 
