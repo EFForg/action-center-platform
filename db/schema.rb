368ce22b95af67d9808a50ee9f6d2ec745c78bfe
vivian <vivian@eff.org>
2018-01-25 16:27:57 -0800

    Store sessions in the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831214704) do
+ActiveRecord::Schema.define(version: 20180124013711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -262,6 +262,16 @@ ActiveRecord::Schema.define(version: 20170831214704) do
     t.integer  "goal"
     t.boolean  "enable_affiliations", default: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "signatures", force: :cascade do |t|
     t.integer  "petition_id"
