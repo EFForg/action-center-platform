e2bc89794b3b121663a5ebecea408a8e849d8824
vivian <vivian@eff.org>
2018-02-05 12:57:39 -0800

    Set session length to a year

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -122,7 +122,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 1.year
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
