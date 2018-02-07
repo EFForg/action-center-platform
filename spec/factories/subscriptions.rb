c845aa1264f54f3a3ba3c446a899ae14e990fa97
K. Stewart <ignitiant@gmail.com>
2018-02-07 15:06:05 -0800

    Add subscription deduplication to signatures: deduplicate task
    https://github.com/EFForg/action-center-platform/issues/336

---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :subscription do
+    first_name "John"
+    last_name "Doe"
+    sequence(:email) { |n| "signer-#{n}@example.com" }
+    partner {|a| a.association :partner }
+  end
+end
--- spec/tasks/signatures_spec.rb
+++ spec/tasks/signatures_spec.rb
@@ -8,6 +8,8 @@ describe "signatures namespace rake tasks" do
   end
 
   describe "signatures:deduplicate" do
+    after { Rake.application["signatures:deduplicate"].reenable }
+
     it "should delete signatures with non-unique emails from petitions" do
       regular_petition = FactoryGirl.create(:petition_complete_with_one_hundred_signatures)
 
@@ -26,6 +28,34 @@ describe "signatures namespace rake tasks" do
       expect(regular_petition.signatures.reload.count).to eq(100)
       expect(petition_with_dups.signatures.reload.count).to eq(82)
       expect(petition_with_dups.signatures.reload.pluck(:email)).to contain_exactly(*distinct_emails)
+    end
+
+    context 'with duplicate subscriptions' do
+      let(:subscription) { FactoryGirl.create(:subscription) }
+      let(:email) { subscription.email }
+      let(:partner) { subscription.partner }
+      let!(:owner_subscription2) do
+        FactoryGirl.create(:subscription, email: email)
+      end
+      let!(:partner_subscription2) do
+        FactoryGirl.create(:subscription, partner: partner)
+      end
+      let!(:dup_subscription) do
+        FactoryGirl.create(:subscription, email: email, partner: partner)
+      end
+
+      it 'removes the newer duplicates' do
+        expect(Subscription.where(email: email, partner: partner).count).to eq(2)
+        expect(Subscription.where(email: email).count).to eq(3)
+        expect(Subscription.where(partner: partner).count).to eq(3)
+
+        expect { Rake.application.invoke_task "signatures:deduplicate" }
+          .to change(Subscription, :count).by(-1)
+
+        expect(Subscription.where(email: email, partner: partner).count).to eq(1)
+        expect(Subscription.where(email: email).count).to eq(2)
+        expect(Subscription.where(partner: partner).count).to eq(2)
+      end
     end
   end
 end
