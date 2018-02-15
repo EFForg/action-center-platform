1245b69478b7d44e64a65266019428460e78c5d5
K. Stewart <ignitiant@gmail.com>
2018-02-14 17:55:06 -0800

    Rate limit the petitions form
    https://github.com/EFForg/action-center-platform/issues/353

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,17 +1,27 @@
 class Rack::Attack
   throttle('registrations', limit: 10, period: 1.day) do |req|
-    if req.path == '/' && req.post?
-      if req.env['HTTP_X_FORWARDED_FOR']
-        req.env['HTTP_X_FORWARDED_FOR'].split(/\s*,\s*/)[0]
-      else
-        req.ip
-      end
-    end
+    original_user if req.path == '/' && req.post?
   end
 
   throttle('password reset', limit: 10, period: 1.day) do |req|
     if req.path == '/password' && req.params['user']
       req.params['user']['email'].presence
+    end
+  end
+
+  throttle('petition signatures', limit: 50, period: 1.day) do |req|
+    original_user if req.path == '/tools/petition' && req.post?
+  end
+
+  private
+
+  def original_user
+    # On Production, we route all traffic through Fastly.
+    # Check the HTTP_X_FORWARDED_FOR header to get the originating IP.
+    if req.env['HTTP_X_FORWARDED_FOR']
+      req.env['HTTP_X_FORWARDED_FOR'].split(/\s*,\s*/)[0]
+    else
+      req.ip
     end
   end
 end
