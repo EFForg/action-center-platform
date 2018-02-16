8dc004ad27bc54451663ec48f2af4e021fe1edb6
K. Stewart <ignitiant@gmail.com>
2018-02-16 11:49:38 -0800

    Don't try to send mail in Development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,9 @@ Actioncenter::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.delivery_method = :test
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.asset_host = 'http://localhost:3000'
