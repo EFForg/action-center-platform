4729af0f47dea9813c41639c86ba219f4db3f391
K. Stewart <ignitiant@gmail.com>
2018-02-16 16:35:59 -0800

    Fix csrf protection actions

--- app/controllers/csrf_protection_controller.rb
+++ app/controllers/csrf_protection_controller.rb
@@ -2,8 +2,10 @@ class CsrfProtectionController < ActionController::Base
   include ActionView::Helpers::TagHelper
 
   def meta_tags
+    @token = form_authenticity_token
   end
 
   def authenticity_token
+    @token = form_authenticity_token
   end
 end
