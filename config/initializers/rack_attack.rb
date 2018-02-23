ce29fa004759ed3ba3f26a68e09654fada2a7293
Vivian Brown <vivian@eff.org>
2018-02-23 22:14:54 +0000

    Fix rack rack throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 class Rack::Attack
   throttle('registrations', limit: 10, period: 1.day) do |req|
-    original_user if req.path == '/' && req.post?
+    original_user(req) if req.path == '/' && req.post?
   end
 
   throttle('password reset', limit: 10, period: 1.day) do |req|
@@ -10,12 +10,12 @@ class Rack::Attack
   end
 
   throttle('petition signatures', limit: 50, period: 1.day) do |req|
-    original_user if req.path == '/tools/petition' && req.post?
+    original_user(req) if req.path == '/tools/petition' && req.post?
   end
 
   private
 
-  def original_user
+  def self.original_user(req)
     # On Production, we route all traffic through Fastly.
     # Check the HTTP_X_FORWARDED_FOR header to get the originating IP.
     if req.env['HTTP_X_FORWARDED_FOR']
