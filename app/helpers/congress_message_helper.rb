67bf0f84909d6c6445ff16f7a0f8bd2eff3fe193
Vivian Brown <vivian@eff.org>
2018-02-27 00:45:45 -0800

    Remove duplicate values from congress_forms_email_values

--- app/helpers/congress_message_helper.rb
+++ app/helpers/congress_message_helper.rb
@@ -9,19 +9,18 @@ module CongressMessageHelper
     end
   end
 
-  def congress_forms_email_values(campaign, location = {})
+  def congress_forms_email_values(campaign, location)
+    location ||= {}
     {
       "$NAME_FIRST": current_first_name,
       "$NAME_LAST": current_last_name,
-      "$ADDRESS_STREET": current_street_address,
-      "$ADDRESS_CITY": current_city,
       "$EMAIL": current_email,
       "$PHONE": current_user.try(:phone),
       "$SUBJECT": campaign.subject,
+      "$ADDRESS_STREET": location[:street] || current_street_address,
+      "$ADDRESS_CITY": location[:city] || current_city,
       "$ADDRESS_ZIP4": location[:zip4],
       "$ADDRESS_ZIP5": location[:zip5],
-      "$ADDRESS_STREET": location[:street],
-      "$ADDRESS_CITY": location[:city],
       "$STATE": location[:state]
     }
   end
