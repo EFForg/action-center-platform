e09fe1ecd45d7fe91d4cec4513239fbaf6cb76f7
Peter Woo <peterw@eff.org>
2018-03-02 17:02:06 -0800

    Disable csrf elsewhere

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   invisible_captcha only: :create
   after_filter :set_create_notice, only: :create
 
+  skip_before_filter :verify_authenticity_token, only: [:create]
+
   # POST /resource
   def create
     super do |resource|
