93a64e7e87532c8cc4a553f5d4ab4094b60a27f6
Peter Woo <peterw@eff.org>
2018-03-12 14:21:32 -0700

    Disable csrf protection on embed.js

--- app/controllers/action_page_controller.rb
+++ app/controllers/action_page_controller.rb
@@ -8,6 +8,8 @@ class ActionPageController < ApplicationController
   before_filter :set_institution, only: [:show_by_institution, :filter]
   before_filter :set_action_display_variables, only: [:show, :show_by_institution, :embed_iframe, :signature_count]
 
+  skip_before_filter :verify_authenticity_token, only: :embed
+
   after_action :allow_iframe, only: :embed_iframe
 
   manifest :action_page
