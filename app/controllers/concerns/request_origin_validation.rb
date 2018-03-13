8286d0e10dc72db9aa9402a74f566b73a30ed38b
Peter Woo <peterw@eff.org>
2018-03-13 11:24:09 -0700

    Prefer double-quoted strings

--- app/controllers/concerns/request_origin_validation.rb
+++ app/controllers/concerns/request_origin_validation.rb
@@ -3,7 +3,7 @@ module RequestOriginValidation
     return if request.get? || request.head?
 
     # Accept blank origin headers because some user agents don't send it.
-    origin = request.headers['origin']
+    origin = request.headers["origin"]
     unless origin.nil? || origin == request.base_url
       logger.warn "HTTP Origin header (#{origin.inspect}) didn't match request.base_url (#{request.base_url})"
       raise ActionController::InvalidCrossOriginRequest
