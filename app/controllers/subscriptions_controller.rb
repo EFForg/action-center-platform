daf6b4307b18fc3440c80651a0cb72cbb6b29081
Vivian Brown <vivian@eff.org>
2019-01-10 18:02:16 -0800

    Add honeypot to newsletter subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,6 +4,7 @@ class SubscriptionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
   before_filter :verify_request_origin, only: :create
 
+  invisible_captcha only: :create
   before_filter :authenticate_user!, only: :edit
 
   def create
