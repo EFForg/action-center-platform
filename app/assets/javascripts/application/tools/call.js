a340a39529e645036670b8bc18a0c92a590ad170
Peter Woo <peterw@eff.org>
2019-02-14 15:52:08 -0800

    Send thank you email to callers who don't subscribe

--- app/assets/javascripts/application/tools/call.js
+++ app/assets/javascripts/application/tools/call.js
@@ -89,9 +89,10 @@ $(document).on('ready', function() {
             fd.append("zipcode", zip_code);
             fd.append("update_user_data", update_user_data);
 
+            fd.append("subscription[email]", form.find("input[type=email]").val());
+
             if (form.find("input[name=subscribe]:checked").val() == "1") {
               fd.append("subscribe", "1");
-              fd.append("subscription[email]", form.find("input[type=email]").val());
               fd.append("subscription[zipcode]", zip_code);
               form.attr("data-signed-up-for-mailings", "true");
             }
