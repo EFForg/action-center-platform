f32c243aec3e6fbbc960c09b81148e08de85d09f
Vivian Brown <vivian@eff.org>
2019-04-01 15:32:34 -0700

    Revert "Log outgoing mail in prod"
    This reverts commit d88d37ec9450fc7f78f070523af7ec8af228bce5.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ Actioncenter::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.logger = Logger.new STDOUT
 
 
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.smtp_domain, :protocol => 'https' }
