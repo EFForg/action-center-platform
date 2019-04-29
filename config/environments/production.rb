c83855272bc22399bca480265bc4829a3ec6bc50
Syd Young <sky@eff.org>
2019-04-29 14:06:57 -0700

    Fix asset precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Actioncenter::Application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
