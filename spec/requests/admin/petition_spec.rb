dc7a313f971ee50f94ebcea07c671ad8dcafef3b
Syd Young <sky@eff.org>
2019-04-29 16:53:26 -0700

    Remove unused let in admin action pages request spec

--- spec/requests/admin/petition_spec.rb
+++ spec/requests/admin/petition_spec.rb
@@ -1,19 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Admin Action Pages", type: :request do
-  let(:valid_attributes) do
-    {
-      action_page: {
-        title: "Save Kittens",
-        summary: "Save kittens in great detail",
-        description: "Some description",
-        email_text: "",
-        enable_tweet: "0"
-      },
-      add_targets: ""
-    }
-  end
-
   describe "admins" do
     before(:each) do
       @admin = FactoryGirl.create(:admin_user)
