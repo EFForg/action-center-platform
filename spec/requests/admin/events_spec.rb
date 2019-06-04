623904d7011884a453b8218157eaebbeefac139f
Vivian Brown <vivian@eff.org>
2019-06-03 17:46:35 -0700

    Define test var with let

--- spec/requests/admin/events_spec.rb
+++ spec/requests/admin/events_spec.rb
@@ -1,12 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Admin Action Page Analytics", type: :request do
-  before(:each) do
-    admin = FactoryGirl.create(:admin_user)
-    login admin
-
-    @action_page = FactoryGirl.create(:action_page_with_views)
-  end
+  let(:action_page) { FactoryGirl.create(:action_page_with_views) }
+  before { login FactoryGirl.create(:admin_user) }
 
   describe "#index" do
     context "with type param" do
@@ -16,7 +12,7 @@ RSpec.describe "Admin Action Page Analytics", type: :request do
               and_return(Time.local(2019)).
               at_least(:once)
 
-        get "/admin/action_pages/#{@action_page.slug}/events",
+        get "/admin/action_pages/#{action_page.slug}/events",
           params: { type: "views" },
           headers: { "ACCEPT" => "application/json" }
 
@@ -30,7 +26,7 @@ RSpec.describe "Admin Action Page Analytics", type: :request do
       it "filters by date" do
         start_date = Time.utc(2019, 1, 1).strftime("%Y-%m-%d")
         end_date = Time.utc(2019, 1, 7).strftime("%Y-%m-%d")
-        get "/admin/action_pages/#{@action_page.slug}/events",
+        get "/admin/action_pages/#{action_page.slug}/events",
           params: { date_start: start_date, date_end: end_date, type: "views" },
           headers: { "ACCEPT" => "application/json" }
 
@@ -51,9 +47,8 @@ RSpec.describe "Admin Action Page Analytics", type: :request do
 
     context "without type param" do
       it "returns daily counts for all event types" do
-        # @TODO @action_page => action_page
-        @action_page.update_attribute(:enable_petition, true)
-        get "/admin/action_pages/#{@action_page.slug}/events",
+        action_page.update_attribute(:enable_petition, true)
+        get "/admin/action_pages/#{action_page.slug}/events",
           headers: { "ACCEPT" => "application/json" }
 
         expect(response.code).to eq "200"
@@ -66,7 +61,7 @@ RSpec.describe "Admin Action Page Analytics", type: :request do
 
   describe "#views" do
     it "displays views in a table" do
-      get "/admin/action_pages/#{@action_page.slug}/views"
+      get "/admin/action_pages/#{action_page.slug}/views"
       expect(response.code).to eq "200"
     end
   end
