13e3a6952be85700aad3b164d2e7d7ba75fa17cc
Vivian Brown <vivian@eff.org>
2019-06-17 18:16:15 -0700

    Mock out backend congress form workflow

---
+++ app/controllers/congress_message_controller.rb
@@ -0,0 +1,27 @@
+class CongressMessageController < ApplicationController
+  def lookup
+    # params: street addr, zip, message, action_page_id
+    @members = CongressMember.lookup(params[street], params[zipcode])
+    @forms = CongressForms::Form.find(@members.pluck(:bioguide_id))
+    # @forms = CongressForms::Form.group_common_fields(forms)
+    # @TODO address, message to template vars for form render, possibly hidden fields
+  end
+
+  def create
+    # @TODO cache b/t requests? store locally?
+    @forms = CongressForms::Form.find(params[bioguide_ids])
+    @forms.each do |f|
+      unless f.validate(congress_message_params)
+        # Re-render form and return
+      end
+    end
+    @forms.each do |f|
+      unless f.submit(congress_message_params)
+        # re-render form and return
+      end
+    end
+  end
+
+  def congress_message_params
+  end
+end
--- lib/congress_forms.rb
+++ lib/congress_forms.rb
@@ -1,6 +1,69 @@
 require "rest_client"
 
 module CongressForms
+  class Form
+    attr_accessor :fields
+
+    def self.find(bioguide_ids)
+      # POST /retrieve-form-elements
+      result = []
+      fields_list.each do |fields|
+        result << Form.new(fields)
+      end
+      # Returns an array of Forms
+      return result
+    end
+
+    def self.group_common_fields(fields_list)
+
+    end
+
+    def initialize(fields)
+      @fields = fields
+    end
+
+    def validate(data)
+      # [
+      #   {
+      #     maxlength: int,
+      #     value: string,
+      #     options_hash: hash
+      #   },
+      #     ...
+      # ]
+      @fields.each do |f|
+        return false unless f.validate(data[f.value])
+      end
+      true
+    end
+
+    def fill(message)
+      params = {}
+      @fields.each do |f|
+        params[f.value] = f.get_input(data)
+      end
+      CongressMessage.post("/fill-out-form/", params)
+    end
+  end
+
+  class Field
+    attr_accessor :max_length, :value, :options_hash
+
+    def initialize(max_length, value, options_hash)
+      @max_length = max_length
+      @value = value
+      @options_hash = options_hash
+    end
+
+    def validate(input)
+      return false if input.nil?
+      return false if @max_length && input.length > @max_length
+      # @TODO this can be an array or hash - currently checking hash key.
+      return false if @options_hash && !@options_hash.include?(input)
+      true
+    end
+  end
+
   def self.url(path = "/", params = {}, bioguide_id = nil)
     url = Rails.application.config.congress_forms_url + path
     url += bioguide_id unless bioguide_id.nil?
