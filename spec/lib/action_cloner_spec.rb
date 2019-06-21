7cd6817875f3b9860c8ba48b468a0f4ec34e8b6d
Syd Young <sky@eff.org>
2019-06-21 16:02:43 -0700

    Draft spec for cloner service object

---
+++ spec/lib/action_cloner_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe ActionCloner do
+  it "does not persist action pages" do
+  end
+
+  it "returns an ActionPage with cloned attributes" do
+  end
+
+  it "returns a draft action page" do
+  end
+
+  it "does not return an archived page" do
+  end
+
+  it "does not return a victory page" do
+  end
+
+  context "duplicating campaign associations" do
+    xit "tweet association" 
+    xit "email association"
+    xit "petition"
+    xit "congress message"
+    xit "call"
+  end
+end
