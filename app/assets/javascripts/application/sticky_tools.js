6715659ce2734a036363ae28cffed716f54052be
Schatzkin <schatzkin@eff.org>
2019-06-27 13:15:09 -0700

    trying out some new sticky code for #tools

--- app/assets/javascripts/application/sticky_tools.js
+++ app/assets/javascripts/application/sticky_tools.js
@@ -1,9 +1,8 @@
 $(window).scroll(function(){
-    if($(window).scrollTop() >= '100'){
+    if( ($(window).scrollTop() >= '30') && ($(window).scrollTop() <= '500')){
         $('.tools-wrapper').removeClass('floating');
         $('.tools-wrapper').addClass('fixed');
     }
-
     else{
         $('.tools-wrapper').addClass('floating');
         $('.tools-wrapper').removeClass('fixed');
