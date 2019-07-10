08eec7e870622780dbbca637c70777d8a69f01b9
Vivian Brown <vivian@eff.org>
2019-07-10 11:46:27 -0400

    Show error on failed fill

--- app/assets/javascripts/application/tools/congress_message.js
+++ app/assets/javascripts/application/tools/congress_message.js
@@ -1,21 +1,21 @@
 $(document).on("ready", function() {
-  $(".congress-message-rep-lookup").on("ajax:beforeSend", function() {
+  $(".congress-message-rep-lookup, #congress-message-create").on("ajax:beforeSend", function() {
     submit_in_progress($(this));
   });
 
   $(".congress-message-rep-lookup").on("ajax:complete", function(xhr, data, status) {
     $form = $(this);
     if (status == "success") {
-      $form.hide();
+      $form.remove();
       $(".congress-message-tool-container").html(data.responseText);
       // Trigger phone field formatting.
       $phone = $(".bfh-phone")
       $phone.bfhphone($phone.data());
       // @TODO auto-set similar fields, eg name prefix, when the first is set.
     } else if (data.responseText) {
-      show_lookup_error(data.responseText, $form);
+      show_error(data.responseText, $form);
     } else {
-      show_lookup_error("Something went wrong. Please try again later.", $form);
+      show_error("Something went wrong. Please try again later.", $form);
     }
   });
 
@@ -24,6 +24,10 @@ $(document).on("ready", function() {
       if (status == "success") {
         $("#congress-message-create").hide();
         $(".email-tool-success-share").html(data.responseText).show();
+      } else if (data.responseText) {
+        show_error(data.responseText, $form);
+      } else {
+        show_error("Something went wrong. Please try again later.", $form);
       }
     }
   });
@@ -34,11 +38,11 @@ $(document).on("ready", function() {
     $("input,textarea,button,select", form).attr("disabled", "disabled");
   }
 
-  function show_lookup_error(error, form) {
+  function show_error(error, form) {
     form.find(".progress-striped").hide();
     form.find("input[type=submit]").show();
     form.find(".alert-danger").remove();
-    $("#lookup-address h3").after($('<div class="small alert alert-danger help-block">').text(error));
+    $("#errors").append($('<div class="small alert alert-danger help-block">').text(error));
     $("input,textarea,button,select", form).removeAttr("disabled");
   }
 
