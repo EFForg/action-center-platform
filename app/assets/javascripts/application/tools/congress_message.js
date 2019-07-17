b5e0ba355b46e92b8c6e8ace48ee3e7553dd184f
Schatzkin <schatzkin@eff.org>
2019-07-17 15:24:55 -0700

    fixing form progress to only show for congress forms

--- app/assets/javascripts/application/tools/congress_message.js
+++ app/assets/javascripts/application/tools/congress_message.js
@@ -1,5 +1,7 @@
 $(document).on("ready", function() {
-
+  if ($('#congress-message-tool').length) {
+    $('.page-indicator').css('display','grid');
+  }
   var emailsSent = 0;
   var allEmailSent = function () {
     $(".step2-intro").delay(500).slideUp(null, height_changed);
@@ -11,9 +13,10 @@ $(document).on("ready", function() {
     $('#congress-message-tool').hide();
     $('#thank-you').show();
     if ($('#thank-you').length ) {
-      $("#tools .page-indicator div.page2").css('background-color', '#EAEAEA');
-      $("#tools .page-indicator div.page3").css('background-color', '#ec1e1e');
+        $("#tools .page-indicator div.page2").css('background-color', '#EAEAEA');
+        $("#tools .page-indicator div.page3").css('background-color', '#ec1e1e');
     }
+
     var action_id = $("[data-action-id]").attr("data-action-id");
     var url = "/tools/message-congress?action_id=" + action_id;
     $.ajax({
@@ -62,172 +65,172 @@ $(document).on("ready", function() {
     var congressFormsUrl = $(".congress-message-tool-container").data("congress-forms-url")
 
     $(".congress-message-tool-container").congressForms({
-        bioguide_ids: rep_ids,
-        labels: false,
-        contactCongressServer: congressFormsUrl,
-        values: emailValues || {},
-        topic_category: options.topicCategory,
-        campaign_tag: options.campaignTag,
-        legislatorLabelClasses: "legislator-label",
-        submitClasses: "btn action",
-        onRender: function(form) {
+      bioguide_ids: rep_ids,
+      labels: false,
+      contactCongressServer: congressFormsUrl,
+      values: emailValues || {},
+      topic_category: options.topicCategory,
+      campaign_tag: options.campaignTag,
+      legislatorLabelClasses: "legislator-label",
+      submitClasses: "btn action",
+      onRender: function(form) {
 
-          $("html, body").animate({
-            scrollTop: $("#congress-message-tool").offset().top
-          }, 800);
+        $("html, body").animate({
+          scrollTop: $("#congress-message-tool").offset().top
+        }, 800);
 
 
-          $("input[name='$EMAIL']").after($("#email-signup-container").show());
-          $("#congressForms-common-fields").after($(".update-user-data-container").show());
+        $("input[name='$EMAIL']").after($("#email-signup-container").show());
+        $("#congressForms-common-fields").after($(".update-user-data-container").show());
 
-          // if the common fields are bundled with a specific legislator's fields, there's only one legislator and don't display the common fields label.
-          var common_fields_label = '<h3 class="legislator-label common-fields"><em>Common fields </em></h3>';
-          if ($("#congressForms-common-fields.congressForms-legislator-fields").length > 0){
-            common_fields_label = "";
+        // if the common fields are bundled with a specific legislator's fields, there's only one legislator and don't display the common fields label.
+        var common_fields_label = '<h3 class="legislator-label common-fields"><em>Common fields </em></h3>';
+        if ($("#congressForms-common-fields.congressForms-legislator-fields").length > 0){
+          common_fields_label = "";
+        }
+
+        $("#congressForms-common-fields").before('<p class="step2-intro">' + options.extraFieldsExplain + "</p>" + common_fields_label);
+        if ($('.step2-intro').length ) {
+          $("#tools .page-indicator div.page1").css('background-color', '#EAEAEA');
+          $("#tools .page-indicator div.page2").css('background-color', '#ec1e1e');
+        }
+        // Replace the <label>s with <h3>s
+        $(".legislator-label").replaceWith(function () {
+          var attrs = { };
+          $.each($(this)[0].attributes, function(idx, attr) {
+            attrs[attr.nodeName] = attr.nodeValue;
+          });
+          return $("<h3 />", attrs).append($(this).contents());
+        });
+        $.each($("select", ".congress-message-tool-container"), function (ind, el) {
+          var defaultText = "Select an option";
+          if ($(el).attr("id") === "$TOPIC") {
+            defaultText = "Choose a topic";
           }
-
-          $("#congressForms-common-fields").before('<p class="step2-intro">' + options.extraFieldsExplain + "</p>" + common_fields_label);
-          if ($('.step2-intro').length ) {
-            $("#tools .page-indicator div.page1").css('background-color', '#EAEAEA');
-            $("#tools .page-indicator div.page2").css('background-color', '#ec1e1e');
+          if ($(el).attr("id") === "$NAME_PREFIX") {
+            defaultText = "Your prefix";
           }
-          // Replace the <label>s with <h3>s
-          $(".legislator-label").replaceWith(function () {
-              var attrs = { };
-              $.each($(this)[0].attributes, function(idx, attr) {
-                  attrs[attr.nodeName] = attr.nodeValue;
-              });
-              return $("<h3 />", attrs).append($(this).contents());
-          });
-          $.each($("select", ".congress-message-tool-container"), function (ind, el) {
-            var defaultText = "Select an option";
-            if ($(el).attr("id") === "$TOPIC") {
-              defaultText = "Choose a topic";
-            }
-            if ($(el).attr("id") === "$NAME_PREFIX") {
-              defaultText = "Your prefix";
-            }
-            $(el).prepend('<option value="">'+defaultText+"</option>");
+          $(el).prepend('<option value="">'+defaultText+"</option>");
 
-            // if, for topic, we already have a "selected" attribute, don't change it.
-            if ($(el).attr("id") !== "$TOPIC" || $(el)[0].selectedIndex == 1){
-              $(el)[0].selectedIndex = 0;
-            }
-          });
-          $("select[id='$NAME_PREFIX']", ".congress-message-tool-container").change(function(){
-            if ($(this)[0].selectedIndex !== 0){
-              var prefix = $("option:selected", $(this)).text().trim();
-              var that = this;
-              $("select[id='$NAME_PREFIX']", ".congress-message-tool-container").each(function(){
-                if ($(this)[0].selectedIndex == 0){
-                  var fsu = FuzzySetUtil.new_from_select($(this))
-                  var match = fsu.match(prefix)
-                  if (match && match[0].degree >= .75){
-                    $("option", $(this)).each(function(){
-                      if ($(this).text().trim() == match[0].term){
-                        $(this).attr("selected", "selected")
-                      }
-                    })
-                  }
-                }
-              });
-            }
-          });
-          $('[id*="STATE"]').each(function(index, el){
-            $("option", el).removeAttr("selected");
-            if ($('option[value="'+emailValues['$STATE'] +'"]', el).length > 0) {
-              $('option[value="'+emailValues['$STATE'] +'"]', el).prop("selected", true);
-              $(el).hide();
-            } else {
-              var state_abbrev = _.findWhere(STATES.ABBREV, {value: emailValues["$STATE"]});
-              if (state_abbrev){
-                var state_full = _.findWhere(STATES.FULL, {name: state_abbrev.name});
-                if (state_full){
-                  $('option[value="'+ state_full.value + '"]', el).prop("selected", true);
+          // if, for topic, we already have a "selected" attribute, don't change it.
+          if ($(el).attr("id") !== "$TOPIC" || $(el)[0].selectedIndex == 1){
+            $(el)[0].selectedIndex = 0;
+          }
+        });
+        $("select[id='$NAME_PREFIX']", ".congress-message-tool-container").change(function(){
+          if ($(this)[0].selectedIndex !== 0){
+            var prefix = $("option:selected", $(this)).text().trim();
+            var that = this;
+            $("select[id='$NAME_PREFIX']", ".congress-message-tool-container").each(function(){
+              if ($(this)[0].selectedIndex == 0){
+                var fsu = FuzzySetUtil.new_from_select($(this))
+                var match = fsu.match(prefix)
+                if (match && match[0].degree >= .75){
+                  $("option", $(this)).each(function(){
+                    if ($(this).text().trim() == match[0].term){
+                      $(this).attr("selected", "selected")
+                    }
+                  })
                 }
               }
-            }
-          });
-
-          form.append(
-            $('<input id="$ADDRESS_STATE" type="hidden" />').
-              val(emailValues["$STATE"])
-          );
-
-          $.each(reps, function(index, rep){
-              // Add their names
-              $('[data-legislator-id="' + rep.bioguide_id + '"] .legislator-label').html("<em>" + rep.first_name + " " + rep.last_name + " </em>");
             });
-
-          $(".legislator-info-popover").popover();
-
-          $(".congress-message-tool-container").slideDown(200, height_changed);
-          webshims.setOptions("forms", {
-            //show custom styleable validation bubble
-            replaceValidationUI: true,
-            lazyCustomMessages: true
-          });
-
-          //start polyfilling
-
-        },
-        onLegislatorSubmit: function(legislatorId, legislatorFieldSet) {
-          // Subscribe the user if checked
-          $(".newsletter-signup").hide();
-          $(".update-user-data-container").hide();
-          $(".legislator-info-popover").hide();
-          legislatorFieldSet.children("h3").append('<span class="info-circle in-progress">Submitting <i class="icon-spin4 animate-spin"></i></span>');
-          $("#congressForms-common-fields .form-group").slideUp(400, height_changed);
-          $(".form-group", legislatorFieldSet).slideUp(400, height_changed);
-          $("input[type=submit]").slideUp(400, height_changed);
-          setTimeout(function () {
-            emailsSent++;
-            if (emailsSent === rep_ids.length) {
-              allEmailSent();
+          }
+        });
+        $('[id*="STATE"]').each(function(index, el){
+          $("option", el).removeAttr("selected");
+          if ($('option[value="'+emailValues['$STATE'] +'"]', el).length > 0) {
+            $('option[value="'+emailValues['$STATE'] +'"]', el).prop("selected", true);
+            $(el).hide();
+          } else {
+            var state_abbrev = _.findWhere(STATES.ABBREV, {value: emailValues["$STATE"]});
+            if (state_abbrev){
+              var state_full = _.findWhere(STATES.FULL, {name: state_abbrev.name});
+              if (state_full){
+                $('option[value="'+ state_full.value + '"]', el).prop("selected", true);
+              }
             }
-            $("#congressForms-common-fields .form-group").slideUp(400, height_changed);
-            $(".form-group", legislatorFieldSet).slideUp(400, height_changed);
-            legislatorFieldSet.find("span.info-circle").replaceWith('<span class="info-circle success">Sent <i class="icon-ok-circle"></i></span>');
-          }, 2000);
-        },
-        onLegislatorSuccess: function(legislatorId, legislatorFieldSet) {
-          // currently no-op
-        },
-        onLegislatorError: function(legislatorId, legislatorFieldSet) {
+          }
+        });
+
+        form.append(
+          $('<input id="$ADDRESS_STATE" type="hidden" />').
+          val(emailValues["$STATE"])
+        );
+
+        $.each(reps, function(index, rep){
+          // Add their names
+          $('[data-legislator-id="' + rep.bioguide_id + '"] .legislator-label').html("<em>" + rep.first_name + " " + rep.last_name + " </em>");
+        });
+
+        $(".legislator-info-popover").popover();
+
+        $(".congress-message-tool-container").slideDown(200, height_changed);
+        webshims.setOptions("forms", {
+          //show custom styleable validation bubble
+          replaceValidationUI: true,
+          lazyCustomMessages: true
+        });
+
+        //start polyfilling
+
+      },
+      onLegislatorSubmit: function(legislatorId, legislatorFieldSet) {
+        // Subscribe the user if checked
+        $(".newsletter-signup").hide();
+        $(".update-user-data-container").hide();
+        $(".legislator-info-popover").hide();
+        legislatorFieldSet.children("h3").append('<span class="info-circle in-progress">Submitting <i class="icon-spin4 animate-spin"></i></span>');
+        $("#congressForms-common-fields .form-group").slideUp(400, height_changed);
+        $(".form-group", legislatorFieldSet).slideUp(400, height_changed);
+        $("input[type=submit]").slideUp(400, height_changed);
+        setTimeout(function () {
           emailsSent++;
           if (emailsSent === rep_ids.length) {
             allEmailSent();
           }
+          $("#congressForms-common-fields .form-group").slideUp(400, height_changed);
           $(".form-group", legislatorFieldSet).slideUp(400, height_changed);
           legislatorFieldSet.find("span.info-circle").replaceWith('<span class="info-circle success">Sent <i class="icon-ok-circle"></i></span>');
-          // All emails are considered sent, even if the server fails
-          //legislatorFieldSet.find('span.info-circle').replaceWith('<span class="info-circle error">Error <i class="icon-error-alt"></i></span>');
-        },
-        onDefunctLegislator: function(bioguide, contact_url) {
-          var notice = $("<p>").addClass("defunct-notice")
-              .text("Sorry, we can't message this legislator at the moment. We are working to fix the problem.");
-
-          if (contact_url) {
-            var link = $("<a>").attr({href: contact_url, target: "_blank"}).text("their website");
-            notice.append(" Please contact them at ", link, " instead.");
-          }
-
-          var fieldset = $("fieldset[data-legislator-id="+bioguide+"]");
-
-          if (!fieldset.length) {
-            fieldset = $('<fieldset>').attr({
-              'data-legislator-id': bioguide,
-              'class': 'congressForms-legislator-fields defunct',
-            });
-
-            $('<label>').addClass('legislator-label').appendTo(fieldset);
-            fieldset.insertBefore('.congress-message-tool-container input[type=submit]');
-          }
-
-          fieldset.prop("disabled", true);
-          fieldset.find(".form-group").remove();
-          fieldset.append(notice);
+        }, 2000);
+      },
+      onLegislatorSuccess: function(legislatorId, legislatorFieldSet) {
+        // currently no-op
+      },
+      onLegislatorError: function(legislatorId, legislatorFieldSet) {
+        emailsSent++;
+        if (emailsSent === rep_ids.length) {
+          allEmailSent();
         }
+        $(".form-group", legislatorFieldSet).slideUp(400, height_changed);
+        legislatorFieldSet.find("span.info-circle").replaceWith('<span class="info-circle success">Sent <i class="icon-ok-circle"></i></span>');
+        // All emails are considered sent, even if the server fails
+        //legislatorFieldSet.find('span.info-circle').replaceWith('<span class="info-circle error">Error <i class="icon-error-alt"></i></span>');
+      },
+      onDefunctLegislator: function(bioguide, contact_url) {
+        var notice = $("<p>").addClass("defunct-notice")
+        .text("Sorry, we can't message this legislator at the moment. We are working to fix the problem.");
+
+        if (contact_url) {
+          var link = $("<a>").attr({href: contact_url, target: "_blank"}).text("their website");
+          notice.append(" Please contact them at ", link, " instead.");
+        }
+
+        var fieldset = $("fieldset[data-legislator-id="+bioguide+"]");
+
+        if (!fieldset.length) {
+          fieldset = $('<fieldset>').attr({
+            'data-legislator-id': bioguide,
+            'class': 'congressForms-legislator-fields defunct',
+          });
+
+          $('<label>').addClass('legislator-label').appendTo(fieldset);
+          fieldset.insertBefore('.congress-message-tool-container input[type=submit]');
+        }
+
+        fieldset.prop("disabled", true);
+        fieldset.find(".form-group").remove();
+        fieldset.append(notice);
+      }
     });
   }
   if ($("#congress-message-tool").length > 0) {
@@ -245,7 +248,7 @@ $(document).on("ready", function() {
       $("form.congress-message-rep-lookup").html("");
 
       var bioguide_ids = tool.data("bioguide-ids").split(",")
-        .map(function(x) { return x.trim() });
+      .map(function(x) { return x.trim() });
       generateCongressForm({
         reps: _.map(bioguide_ids, function(bioguide_id) {
           return {
@@ -279,12 +282,12 @@ $(document).on("ready", function() {
       $("input,textarea,button,select", form).attr("disabled", "disabled");
 
       SmartyStreets.street_address({ street: address,
-                                     zipcode: zip })
+        zipcode: zip })
         .done(function(smart_data) {
           if (smart_data.length > 0) {
             var zip4 = smart_data[0].components.plus4_code;
             if (!zip4)
-              return show_address_error(App.Strings.addressLookupFailed);
+            return show_address_error(App.Strings.addressLookupFailed);
 
             var state = smart_data[0].components.state_abbreviation;
             var city = smart_data[0].components.city_name;
@@ -305,10 +308,10 @@ $(document).on("ready", function() {
                 } else {
                   var data = _.filter(data, function (rep) {
                     if (rep.chamber === "senate" && tool.data("target-senate"))
-                      return true;
+                    return true;
 
                     if (rep.chamber === "house" && tool.data("target-house"))
-                      return true;
+                    return true;
 
                     return false;
                   });
@@ -331,8 +334,8 @@ $(document).on("ready", function() {
             $("input,textarea,button,select", form).removeAttr("disabled");
             show_address_error(App.Strings.addressLookupFailed);
           }
-      });
-    };
-    return false;
+        });
+      };
+      return false;
+    });
   });
-});
