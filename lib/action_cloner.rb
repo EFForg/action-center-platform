64b9ba946aef8640e7c6f468d14170f04776852b
Vivian Brown <vivian@eff.org>
2019-07-17 15:39:18 -0700

    Use ActiveRecord#dup for cloning

--- lib/action_cloner.rb
+++ lib/action_cloner.rb
@@ -1,54 +1,22 @@
-class ActionCloner
-  def self.run(*args)
-    new(*args).run
-  end
-
-  def initialize(action_page)
-    @attrs = action_page.attributes.symbolize_keys
-    @page = action_page
-  end
-
-  def run
-    clean_attributes
-    @clone = ActionPage.new(attrs)
-    clone_campaign
-    clone
+module ActionCloner
+  def self.run(original)
+    clone = original.dup
+    clone.published = false
+    clone.archived = false
+    clone_campaign(original, clone)
   end
 
   private
 
-  attr_reader :attrs, :clone, :page
-
-  def clean_attributes
-    unpublish
-    unarchive
-    @attrs = remove_attrs(@attrs)
-  end
-
-  def unpublish
-    attrs[:published] = false
-  end
-
-  def unarchive
-    attrs[:archived] = false
-  end
-
-  def remove_attrs(hash)
-    to_remove = %i(id created_at updated_at slug tweet_id email_campaign_id
-                   call_campaign_id petition_id congress_message_campaign_id)
-    to_remove.each { |a| hash.delete a }
-    hash
-  end
-
-  def clone_campaign
-    campaign_sym, model = determine_campaign
-    return unless campaign_sym && model
+  def self.clone_campaign(page, clone)
+    campaign_sym, model = determine_campaign(page)
+    return clone unless campaign_sym && model
     campaign = page.send(campaign_sym)
-    campaign_attrs = remove_attrs(campaign.attributes.symbolize_keys)
-    clone.send("#{campaign_sym}=", model.new(campaign_attrs))
+    clone.send("#{campaign_sym}=", campaign.dup)
+    clone
   end
 
-  def determine_campaign
+  def self.determine_campaign(page)
     return [:tweet, Tweet] if page.enable_tweet?
     return [:email_campaign, EmailCampaign] if page.enable_email?
     return [:petition, Petition] if page.enable_petition?
