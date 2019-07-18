432937de073cd6496ebcf8f4fb39253b6e1fa64f
Vivian Brown <vivian@eff.org>
2019-07-18 15:47:42 -0700

    Dupe all tools when cloning an action page

--- lib/action_cloner.rb
+++ lib/action_cloner.rb
@@ -3,24 +3,17 @@ module ActionCloner
     clone = original.dup
     clone.published = false
     clone.archived = false
-    clone_campaign(original, clone)
+    clone_tools(original, clone)
   end
 
   private
 
-  def self.clone_campaign(page, clone)
-    campaign_sym, model = determine_campaign(page)
-    return clone unless campaign_sym && model
-    campaign = page.send(campaign_sym)
-    clone.send("#{campaign_sym}=", campaign.dup)
+  def self.clone_tools(original, clone)
+    %i(tweet email_campaign petition congress_message_campaign call_campaign).each do |tool_sym|
+      if original.send("#{tool_sym}_id").present?
+        clone.send("#{tool_sym}=", original.send(tool_sym).dup)
+      end
+    end
     clone
-  end
-
-  def self.determine_campaign(page)
-    return [:tweet, Tweet] if page.enable_tweet?
-    return [:email_campaign, EmailCampaign] if page.enable_email?
-    return [:petition, Petition] if page.enable_petition?
-    return [:congress_message_campaign, CongressMessageCampaign] if page.enable_congress_message?
-    return [:call_campaign, CallCampaign] if page.enable_call?
   end
 end
