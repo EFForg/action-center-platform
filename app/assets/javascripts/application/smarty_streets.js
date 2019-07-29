c7aab818590870ce3645a86b069386ded6d898b1
Vivian Brown <vivian@eff.org>
2019-07-29 14:10:24 -0700

    Remove SmartyStreets js wrapper

--- app/assets/javascripts/application/smarty_streets.js
+++
@@ -1,9 +0,0 @@
-SmartyStreets = {
-  // Given an address+zip, this will check that the address exists and
-  // return a zip+4 and most importantly a congressional district for
-  // pulling up relevant senators/ congress persons
-  street_address: function(data) {
-    var url = '/smarty_streets/street_address/';
-    return $.get(url, data);
-  }
-};
