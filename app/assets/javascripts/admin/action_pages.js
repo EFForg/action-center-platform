686609004ff0de14daf5ad6e1e3c27297bcbf28d
Peter Woo <peterw@eff.org>
2019-07-29 10:37:11 -0700

    Fix form initialization for external link actions

--- app/assets/javascripts/admin/action_pages.js
+++ app/assets/javascripts/admin/action_pages.js
@@ -29,15 +29,15 @@ $('.action_pages-edit, .action_pages-new').on('change', 'input[name=action_type]
   reflowEpicEditor();
 });
 
-$(function() {
-  $('.action_pages-edit, .action_pages-new').find('input[name=action_type]').trigger('change');
-});
-
 $('.action_pages-edit, .action_pages-new').on('change', 'input[name=action_type]', function(e) {
   if (e.target.value == 'redirect')
     $('#nav li[data-hide-for-redirect]').slideUp(100);
   else
     $('#nav li[data-hide-for-redirect]').slideDown();
+});
+
+$(function() {
+  $('.action_pages-edit, .action_pages-new').find('input[name=action_type]:checked').trigger('change');
 });
 
 $('.action_pages-edit, .action_pages-new').on('click', '#nav a[href=#save]', function(e) {
