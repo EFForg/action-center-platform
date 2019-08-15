47a3415f78bac00c168032b96656d1b9bc6e10d0
Schatzkin <schatzkin@eff.org>
2019-08-15 13:38:38 -0700

    page indictor for page 1 & 2 working

--- app/assets/javascripts/application/tools/congress_message.js
+++ app/assets/javascripts/application/tools/congress_message.js
@@ -4,16 +4,33 @@ $(document).on("ready", function() {
     $("input[type=submit]").hide();
     $("input,textarea,button,select", $(this)).attr("disabled", "disabled");
   });
-  $("#to-page-2").click(function(){
+  $("#to-page-2").click(function(){  // show customize message
     $("#customize-message").show();
     $(".address-lookup").hide();
     $("#to-page-2").hide();
+    $(".page-indicator div.page1").css({
+      'background-color': '#2D2D2D',
+      'color': '#BABABA'
+    });
+    $(".page-indicator div.page2").css({
+      'background-color': '#BABABA',
+      'color': '#2D2D2D'
+    });
   });
 
-  $("#to-page-3").click(function(){
+  $("#to-page-3").click(function(){ // show representatives info
     $("#customize-message").hide();
     $("#to-page-3").hide();
+    $(".page-3").css({
+      'background-color': '#BABABA',
+      'color': '#2D2D2D'
+    })
   });
+
+  if ($("#congress-message-tool").length){  // showing paging at top of form
+    $(".page-indicator").css("display", "grid");
+  }
+
 
 
   $(".congress-message-rep-lookup").on("ajax:complete", function(xhr, data, status) {
