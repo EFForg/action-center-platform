e674e69e03e948d02e14ef77203ea43646369edd
Syd Young <sky@eff.org>
2019-11-19 14:16:56 -0800

    Use a better condition for limiting events to after launch

--- app/controllers/admin/action_pages_controller.rb
+++ app/controllers/admin/action_pages_controller.rb
@@ -129,9 +129,8 @@ class Admin::ActionPagesController < Admin::ApplicationController
     respond_to do |format|
       format.html do
         if @actionPage.enable_congress_message?
-          launch_date = Time.zone.parse("Nov 19 2019")
           action_events = @actionPage.events.where(name: 'Action')
-                                            .where("time > ?", launch_date)
+                                     .where("json_extract_path(properties, 'customizedMessage') is not null")
           @total = action_events.count
           @customized = action_events.where("properties ->> 'customizedMessage' = 'true'").count
           @percentage = @total != 0 ? (@customized / @total.to_f) * 100 : 0
