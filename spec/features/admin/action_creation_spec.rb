84048e3abc3407d72c1c76d2743ff70a93519908
Syd Young <sky@eff.org>
2019-12-05 14:22:00 -0800

    Add the rest of the admin creation tests

--- spec/features/admin/action_creation_spec.rb
+++ spec/features/admin/action_creation_spec.rb
@@ -5,46 +5,132 @@ RSpec.describe "Admin action page creation", type: :feature, js: true do
   let!(:category) { FactoryGirl.create(:category, title: "Privacy") }
   it "can create tweet actions" do
     visit new_admin_action_page_path
-
-    fill_in "Title", with: "Tweet Action"
-    set_editor("#action_page_summary", "A summary")
-    set_editor("#action_page_description", "A description")
-    set_select2('#action_page_category_id', category.title)
+    fill_in_basic_info(title: "Very Important Action",
+                       summary: "A summary",
+                       description: "A description")
     click_on "Next"
 
     select_action_type("tweet")
     fill_in "Message", with: "A message"
     click_on "Next"
-    
-    # Skip image
-    click_on "Next"
-    sleep 0.1
-    click_on "Next"
-    sleep 0.1
 
-    fill_in "Share Message", with: "Tweet at your reps"
-    fill_in "Title", with: "A social media title"
+    skip_image_selection
+    fill_in_social_media
+    # Skip partners
     click_on "Next"
 
+    click_on "Save"
+    expect(page).to have_content("Very Important Action")
+  end
+
+  it "can create basic petition actions" do
+    visit new_admin_action_page_path
+    fill_in_basic_info(title: "Very Important Action",
+                       summary: "A summary",
+                       description: "A description")
+    click_on "Next"
+
+    select_action_type("petition")
+    fill_in_editor "#action_page_petition_attributes_description",
+      with: "A petititon letter"
+    fill_in "Goal", with: 1000
+    click_on "Next"
+
+    skip_image_selection
+    fill_in_social_media
     # Skip partners
     click_on "Next"
 
     click_on "Save"
 
-    expect(page).to have_content("Tweet Action")
+    expect(page).to have_content("Very Important Action")
   end
 
-  def set_editor(locator, value)
-    within_frame find(locator, visible: :all).sibling("div").find("iframe") do
-      within_frame find("iframe") do
-        find("body").set(value)
-      end
-    end
+  it "can create email actions" do
+    visit new_admin_action_page_path
+    fill_in_basic_info(title: "Very Important Action",
+                       summary: "A summary",
+                       description: "A description")
+    click_on "Next"
+
+    select_action_type("email")
+    fill_in "To", with: "test@gmail.com"
+    fill_in "Subject", with: "Subject"
+    fill_in "Message", with: "An email"
+    click_on "Next"
+
+    skip_image_selection
+    fill_in_social_media
+    # Skip partners
+    click_on "Next"
+
+    click_on "Save"
+
+    expect(page).to have_content("Very Important Action")
   end
 
-  def set_select2(locator, value)
-    find(locator).sibling('.select2-container').click
-    find('li.select2-results__option[role="treeitem"]', text: value).click
+  it "can create congress actions" do
+    visit new_admin_action_page_path
+    fill_in_basic_info(title: "Very Important Action",
+                       summary: "A summary",
+                       description: "A description")
+    click_on "Next"
+
+    select_action_type("congress_message")
+    fill_in "Subject", with: "Subject"
+    fill_in "Message", with: "A message"
+    click_on "Next"
+
+    skip_image_selection
+    fill_in_social_media
+    # Skip partners
+    click_on "Next"
+
+    click_on "Save"
+
+    expect(page).to have_content("Very Important Action")
+  end
+
+  it "can create call actions" do
+    visit new_admin_action_page_path
+    fill_in_basic_info(title: "Very Important Action",
+                       summary: "A summary",
+                       description: "A description")
+    click_on "Next"
+
+    select_action_type "call"
+    fill_in_editor "#action_page_call_campaign_attributes_message",
+      with: "Call script"
+    click_on "Next"
+
+    skip_image_selection
+    fill_in_social_media
+    # Skip partners
+    click_on "Next"
+
+    click_on "Save"
+
+    expect(page).to have_content("Very Important Action")
+  end
+
+  def fill_in_basic_info(title:, summary:, description:)
+    fill_in "Title", with: title
+    fill_in_editor "#action_page_summary", with: summary
+    fill_in_editor "#action_page_description", with: description
+    fill_in_select2 "#action_page_category_id", with: category.title
+  end
+
+  def skip_image_selection
+    click_on "Next"
+    sleep 0.1
+    click_on "Next"
+    sleep 0.1
+  end
+
+  def fill_in_social_media
+    fill_in "Share Message", with: "Twitter message"
+    fill_in "Title", with: "A social media title"
+    click_on "Next"
   end
 
   def select_action_type(type)
