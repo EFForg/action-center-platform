37436cd79047fead87bd127f54b290e4dacee88d
Peter Woo <peterw@eff.org>
2020-02-18 11:03:42 -0800

    Fix events tests

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -6,7 +6,12 @@ class Admin::EventsController < Admin::ApplicationController
     @events = Ahoy::Event.all.in_range(@start_date, @end_date)
     respond_to do |format|
       format.html { @summary = @events.summary }
-      format.json { render json: @events.chart_data(type: params[:type]) }
+      format.json {
+        render json: @events.chart_data(
+                 type: params[:type],
+                 range: @start_date..@end_date
+               )
+      }
     end
   end
 end
