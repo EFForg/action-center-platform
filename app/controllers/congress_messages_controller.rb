cfd10cbddecc700794444520c61288d5facea675
Peter Woo <peterw@eff.org>
2020-07-16 15:27:35 -0700

    Remove redundant email check

--- app/controllers/congress_messages_controller.rb
+++ app/controllers/congress_messages_controller.rb
@@ -92,7 +92,7 @@ class CongressMessagesController < ApplicationController
   def subscribe_user
     create_partner_subscription
 
-    if params[:subscribe] == "1" && EmailValidator.valid?(user_params[:email])
+    if params[:subscribe] == "1"
       source = "action center congress message :: " + @action_page.title
       user = User.find_or_initialize_by(email: user_params[:email])
       user.attributes = user_params
