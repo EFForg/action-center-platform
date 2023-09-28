5b6e8cf7c812e14fb894545b7628de0f1eea34a7
Syd Young <sky@eff.org>
2023-09-27 23:25:17 -0700

    Modernize signature factory

--- spec/factories/signatures.rb
+++ spec/factories/signatures.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
+  sequence :signer_email do |n|
+    "signer-#{n}@example.com"
+  end
+
   factory :signature do
     first_name { "John" }
     last_name { "Doe" }
-    sequence(:email) { |n| "signer-#{n}@example.com" }
+    email { generate(:signer_email) }
     country_code { "US" }
     zipcode { "94109" }
     street_address { "815 Eddy St" }
