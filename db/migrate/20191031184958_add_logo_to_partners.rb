99befaed5f40c9f380d6702f06d65bee8c863597
Jeff Parr <jeffparr100@gmail.com>
2024-04-17 13:22:23 -0700

    remove logo from partners model

--- db/migrate/20191031184958_add_logo_to_partners.rb
+++ db/migrate/20191031184958_add_logo_to_partners.rb
@@ -1,11 +1,9 @@
 class AddLogoToPartners < ActiveRecord::Migration[5.0]
-  def up
-    add_attachment :partners, :logo
+  def change
+    add_column :action_pages, :logo_file_name, :string
+    add_column :action_pages, :logo_content_type, :string
+    add_column :action_pages, :logo_file_size, :bigint
+    add_column :action_pages, :logo_updated_at, :datetime
     add_column :partnerships, :enable_mailings, :boolean, default: false, null: false
-  end
-
-  def down
-    remove_attachment :partners, :logo
-    remove_column :partnerships, :enable_mailings, :boolean, default: false, null: false
   end
 end
