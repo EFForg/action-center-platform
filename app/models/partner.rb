219fd1511a870fa2f4da41a53f52944eb68aad1a
Jeff Parr <jeffparr100@gmail.com>
2024-04-17 13:22:23 -0700

    remove logo from partners model

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,17 +1,10 @@
 class Partner < ApplicationRecord
-  extend AmazonCredentials
   acts_as_paranoid
   has_many :subscriptions
   has_many :users
   has_many :partnerships
   has_many :action_pages, through: :partnerships
 
-  # todo: doesnt appear to be used?
-  has_attached_file :logo, amazon_credentials
-
-  validates_media_type_spoof_detection :logo,
-                                       if: -> { logo.present? && logo_file_name_came_from_user? }
-  do_not_validate_attachment_file_type [:logo]
   validates :code, uniqueness: true
 
   def to_csv(options = {})
