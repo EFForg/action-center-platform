764669c65d381fe1aab4d47d1a4f3c2776403449
sylph <sky@eff.org>
2025-02-06 11:57:13 -0800

    Add more sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,9 @@
 Sentry.init do |config|
   config.dsn = Rails.application.secrets.sentry_dsn
+  # get breadcrumbs from logs
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # enable tracing
+  # we recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
 end
