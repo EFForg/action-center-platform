b707d961fb8d5f73b9eba60799f6d43ae484e663
Christa Hartsock <christa.hartsock@eff.org>
2025-03-17 11:27:01 -0700

    Scrub potetially sensitive info before sending to Sentry
    Since we intend to send exception, trace, and breadcrumb info to
    a third-party Sentry, we now need to scrub potentially sensitive info
    before sending.
    
    This commit attempts to remove where we've seen it, including:
    * from where we were intentionally sending addtiional data with exceptions
    * removing from breadcrumbs and traces via the Rails filter_parameters, which
    uses regexes to filter data from appearing in logs
    * dropping query params from the path in action-controller breadcrumbs, since
    it was showing get params that could reveal address information.
    
    More might be needed

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :email, :first_name, :last_name, :zipcode, :street, :city, :state, :country_code, :phone, :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn]
+Rails.application.config.filter_parameters += [
+    :password,
+    :email,
+    :first_name,
+    /name_first/i,
+    /name_last/i,
+    :last_name,
+    # :zipcode,
+    :street,
+    :city,
+    :state,
+    :country_code,
+    :phone,
+    :passw,
+    :secret,
+    :token,
+    :_key,
+    :crypt,
+    :salt,
+    :certificate,
+    :otp,
+    :ssn,
+    /\$?message/i,
+]
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,9 +1,73 @@
+# frozen_string_literal: true
+
+require "active_support/parameter_filter"
+
+# https://docs.sentry.io/platforms/ruby/configuration/options/
 Sentry.init do |config|
   config.dsn = Rails.application.secrets.sentry_dsn
   # get breadcrumbs from logs
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # enable tracing
-  # we recommend adjusting this value in production
-  config.traces_sample_rate = 1.0
+  config.environment = ENV['SENTRY_ENVIRONMENT']
+
+  config.send_default_pii = false
+
+  config.before_breadcrumb = lambda do |breadcrumb, hint|
+    # Remove query params from path in request breadcrumbs, since they can share sensitive info like addresses
+    if hint.dig('request').present?
+      breadcrumb.path = begin
+        url = URI.parse(breadcrumb.path)
+        url.query = nil
+        url.to_s
+      rescue URI::Error
+        url.split("?").first
+      end
+    end
+    breadcrumb
+  end
+
+  # Filter out potentially sensitive data before sending to Sentry.io
+  # This applies any filters we've set to logs to also apply to Sentry data
+  #
+  # https://docs.sentry.io/platforms/ruby/guides/rails/configuration/filtering/
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, _hint|
+    # Sanitize extra data
+    if event.extra
+      event.extra = filter.filter(event.extra)
+    end
+    # Sanitize user data
+    if event.user
+      event.user = filter.filter(event.user)
+    end
+    # Sanitize context data (if present)
+    if event.contexts
+      event.contexts = filter.filter(event.contexts)
+    end
+
+    # Return the sanitized event object
+    event
+  end
+
+  # https://docs.sentry.io/platforms/ruby/configuration/sampling/
+  # A sample rate of 0 or false sends nothing, 1.0 or true sends everything
+  config.traces_sampler = lambda do |sampling_context|
+    # if this is the continuation of a trace, just use that decision (rate controlled by the caller)
+    unless sampling_context[:parent_sampled].nil?
+      next sampling_context[:parent_sampled]
+    end
+
+    # /heartbeat
+    if sampling_context.dig(:env, 'HTTP_USER_AGENT') =~ (/healthcheck/i) || sampling_context.dig(:env, 'PATH_INFO') =~ (/heartbeat/i)
+      0.0
+    else
+      begin ENV['SENTRY_TRACES_SAMPLE_RATE'].to_f rescue 0.0 end
+    end
+  end
+
+  # Set profiles_sample_rate to profile 100% of sampled transactions.
+  # We recommend adjusting this value in production.
+  #
+  # Does not send profiling info if set to 0
+  config.profiles_sample_rate = begin ENV['SENTRY_PROFILES_SAMPLE_RATE'].to_f rescue 0 end
 end
